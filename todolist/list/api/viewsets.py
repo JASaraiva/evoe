5a06d024bc47291af61a60d79feb1650fe624c1a
Jonas Almeida <jo.almeida@reply.com>
2020-09-01 15:37:54 -0300

    add authentication and permissions

--- todolist/list/api/viewsets.py
+++ todolist/list/api/viewsets.py
@@ -1,8 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from todolist.list.models import List
 from todolist.list.api.serializers import ListSerializer
 
 class ListViewSet(ModelViewSet):
+
+    permission_classes = [IsAuthenticated] 
+    authentication_classes = [TokenAuthentication] 
 
     queryset = List.objects.all()
     serializer_class = ListSerializer

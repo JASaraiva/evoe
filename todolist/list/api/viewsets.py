2049ca28ce1486c088f4a1f426c3ab5f9a0573f4
Jonas Almeida <jo.almeida@reply.com>
2020-09-02 07:21:54 -0300

    get session user id for new itens

--- todolist/list/api/viewsets.py
+++ todolist/list/api/viewsets.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
 from todolist.list.models import List
 from todolist.list.api.serializers import ListSerializer
+from django.contrib.auth.models import User
 
 class ListViewSet(ModelViewSet):
 
@@ -11,3 +13,17 @@ class ListViewSet(ModelViewSet):
 
     queryset = List.objects.all()
     serializer_class = ListSerializer
+
+
+    def create(self, request):
+
+        item = {"item": request.data["item"],
+           "checked": request.data["checked"],
+           "user": request.user.id,
+        }
+        
+
+        serializer = self.get_serializer(data=item)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data)
